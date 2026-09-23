4b5a70ecc6f9bfad3b213d880b951797875c9f59
bakhit23 <m.bakhit@sd.zain.com>
2026-09-23 16:10:11 +0400

    add tasks

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Welcome to Simple Task Manager!"
+    if request.method == "POST":
+        task = request.form.get("task")
+
+        if task:
+            tasks.append(task)
+
+    return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
