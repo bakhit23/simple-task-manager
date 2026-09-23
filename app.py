3c7df2cb6e922e102b1a00aaa0970fb4fc6d10db
bakhit23 <m.bakhit@sd.zain.com>
2026-09-23 16:39:35 +0400

    add deleate task

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -13,6 +13,15 @@ def home():
             tasks.append(task)
 
     return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:task_id>")
+def delete_task(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
